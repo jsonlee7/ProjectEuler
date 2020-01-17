178434be636af3636b8a501ddc232cdf167e476a
jason.lee <jason.lee@searsfranchises.com>
2020-01-17 08:41:26 -0500

    Add project files.

---
+++ ProjectEuler/Problem1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class Problem1
+    {
+        public static int CalculateSumMultiples3And5() {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Problem1.CalculateSumMultiples3And5());
+        }
+    }
+}
